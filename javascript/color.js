3b2b031b823b0d24fe8e36d8de454a91eb3a0e43
cruickshankatc <cruickshankatc@gmail.com>
2024-05-07 01:07:26 -0400

    Updates for recoloring sky with time of day.

--- javascript/color.js
+++ javascript/color.js
@@ -1,3 +1,23 @@
+let dayColors = {
+  redValue: 25,
+  greenValue: 195,
+  blueValue: 250,
+}
+
+let eveningColors = {
+  redValue: 100,
+  greenValue: 75,
+  blueValue: 150,
+}
+
+let nightColors = {
+  redValue: 0,
+  greenValue: 0,
+  blueValue: 65,
+}
+
+
+
 //Contain the rgb values that will be used within
 //the variable "color" to change the color of the
 //sky
@@ -14,9 +34,23 @@ function colorChange() {
   color = `rgb(${redValue}, ${greenValue}, ${blueValue})`;
   background(color);
   
+  // if (dayTime) {
+  //   blueValue++;
+  // } else if (eveningTime) {
+  //   greenValue++;
+  // }
+  
   if (dayTime) {
-    blueValue++;
+    redValue = dayColors.redValue;
+    greenValue = dayColors.greenValue;
+    blueValue = dayColors.blueValue;
   } else if (eveningTime) {
-    greenValue++;
+    redValue = eveningColors.redValue;
+    greenValue = eveningColors.greenValue;
+    blueValue = eveningColors.blueValue;
+  } else if (nightTime) {
+    redValue = nightColors.redValue;
+    greenValue = nightColors.greenValue;
+    blueValue = nightColors.blueValue;
   }
 }
